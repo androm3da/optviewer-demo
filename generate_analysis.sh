8239089e1a08f0dbf4e6d59a33d93533e67dcb36
Brian Cain <bcain@codeaurora.org>
2017-02-16 19:16:14 -0600

    Switch to -O3 for CPython, Thrift builds

--- generate_analysis.sh
+++ generate_analysis.sh
@@ -13,7 +13,7 @@ build_thrift()
 {
     cd thrift
     ./bootstrap.sh 
-    CXX=clang++-4.0 CC=clang-4.0 CXXFLAGS="-fsave-optimization-record" CFLAGS="-fsave-optimization-record" ./configure 
+    CXX=clang++-4.0 CC=clang-4.0 CXXFLAGS="-O3 -fsave-optimization-record" CFLAGS="-O3 -fsave-optimization-record" ./configure 
     make 
     cd -
 }
@@ -26,7 +26,7 @@ THRIFT_YAML=$(find thrift -name '*.opt.yaml')
 # CPython
 build_cpython()
 {
-    cd cpython; CC=clang-4.0 CFLAGS="-fsave-optimization-record" ./configure --with-optimizations && make ; cd -
+    cd cpython; CC=clang-4.0 CFLAGS="-O3 -fsave-optimization-record" ./configure --with-optimizations && make ; cd -
 }
 
 build_cpython 2>&1 | tee ${OUTPUT}/cpython/build.log
